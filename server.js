143ff54b1510be545f1c88de740d77269a18c1a1
Jackson Neuman <neumanjackson@gmail.com>
2024-02-21 13:13:07 -0600

    added server.js and installed dependencies

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app. use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/fitness-tracker', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
